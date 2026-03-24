09bf4bec3699915c0af7610ead8eea8c786a0992
Jssagalaka <jaydeepsagalaka@gmail.com>
2026-03-24 12:54:40 +0530

    remove repet seaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -241,9 +241,7 @@ export default function Home() {
       </section>
 
       
-      <section id="ProductCatagories">
-        <ProductCategories />
-      </section>
+   
 
       <section className="py-16 px-4 sm:px-6 lg:px-20 bg-gradient-to-b from-white to-green-50">
         <div className="max-w-7xl mx-auto">
